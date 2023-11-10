bb04615912a70109cb0f1746e913a5198edada96
OmegaCri <147819931+OmegaCri@users.noreply.github.com>
2023-11-10 13:52:44 +0100

    Add files via upload

---
+++ Lezione 3/js/script.js
@@ -0,0 +1,44 @@
+alert("Ciao bro");
+
+
+let eta = window.prompt("quanti anni hai?")
+
+/**
+ * Questo è un commento multiriga
+ * 
+ */
+
+
+
+
+if(eta < 18){
+    alert("Mi dispiace ma non puoi accedere alla pagina")
+}else{
+    alert("Benvenuto");
+}
+
+// VARIABILI
+let matricola = 100 //Tipo di dato: Number
+let saluto = "Ciao, mi chiamo Cristian" //Tipo di dato: String
+let luce = false; //Tipo di dato: Boolean
+
+//Stramperemo in console i tre valori
+//Scrive in console il valore della variabile
+console.log(saluto);
+
+console.log("La mia matricola è:" + matricola);
+
+console.log("Luce:"+ luce);
+
+
+let A = 5;
+let B = 3;
+
+let somma = A + B;
+
+
+console.log("La somma vale" + A + B);
+console.log(A + B);
+
+
+//Uniamo i tre valori per poi stamparli
